9a9d1815c1cfc05598d144ca4f6fb9860fd4ec62
tkachenkosergey <43620917+tkachenkosergey@users.noreply.github.com>
2018-12-27 06:12:10 +0000

    Update naivniy.r

--- naivniy.r
+++ naivniy.r
@@ -1,5 +1,5 @@
 naive = function(x, Py, mu, sigm, m, n) {
-  amo <- matrix(c('setosa','versicolor', 'virginica', 0, 0, 0), nrow = 3, ncol = 2)
+  gen<- matrix(c('setosa','versicolor', 'virginica', 0, 0, 0), nrow = 3, ncol = 2)
   scores = rep(0, m)
   for (i in 1:m) {
     scores[i] = Py[i]
@@ -7,9 +7,9 @@ naive = function(x, Py, mu, sigm, m, n) {
       N=1/sqrt(2*pi)/sigm[i,j]*exp(-1/2*(x[j]-mu[i,j])^2/sigm[i,j]^2)
       scores[i] = scores[i] * N
     }
-    amo[i,2]=scores[i]
+    gen[i,2]=scores[i]
   }
-  class <- amo[,1][which.max(amo[,2])]
+  class <- gen[,1][which.max(amo[,2])]
 }
 
 
